af56f8dc62dd185467537b042c6ecc64aacc47d6
carnegie3500 <carnegie3500@gmail.com>
2019-07-09 15:11:55 +0900

    saldp
    asdasd

---
+++ contents.php
@@ -0,0 +1,135 @@
+<!-- 내용영역 -->
+<section id="contents" class="container-fluid">
+	<article class="container content1">
+		<ul class="nav nav-tabs nav-justified">
+			<li class="active">
+				<h3><a href="#box" data-toggle="tab">박스오피스</a></h3>	
+			</li>
+			<li>
+				<h3><a href="#latest" data-toggle="tab">최신개봉작</a></h3>
+			</li>
+			<li>
+				<h3><a href="#open" data-toggle="tab">상영예정작</a></h3>
+			</li>
+		</ul>
+		<div class="tab-content">
+			<ul id="box" class="tab-pane fade in active">
+				<li class="col-sm-6 col-md-3">
+					<a href="#">
+						<img src="img/box1.jpg">
+						<h4>알라딘</h4>
+					    <span>+상세보기</span>
+					</a>
+					<button class="btn btn-default">예매하기</button>
+				</li>
+				<li class="col-sm-6 col-md-3">
+					<a href="#">
+						<img src="img/box2.jpg">
+						<h4>토이스토리 4</h4>
+					    <span>+상세보기</span>
+					</a>
+					<button class="btn btn-default">예매하기</button>	
+				</li>
+				<li class="col-sm-6 col-md-3">
+					<a href="#">
+						<img src="img/box3.jpg">
+						<h4>롱 리브 더 킹:목포 영웅</h4>
+					    <span>+상세보기</span>
+					</a>
+					<button class="btn btn-default">예매하기</button>
+				</li>
+				<li class="col-sm-6 col-md-3">
+					<a href="#">
+						<img src="img/box4.jpg">
+						<h4>기생충</h4>
+					    <span>+상세보기</span>
+					</a>
+					<button class="btn btn-default">예매하기</button>
+				</li>
+			</ul>
+			<ul id="latest" class="tab-pane fade">
+				<li class="col-sm-6 col-md-3"><a href="#">
+						<img src="img/latest1.jpg">
+						<h4>에움길</h4>
+					    <span>+상세보기</span>
+					</a>
+					<button type="button" class="btn btn-default">예매하기</button>
+				</li>
+				<li class="col-sm-6 col-md-3">
+					<a href="#">
+						<img src="img/latest2.jpg">
+						<h4>사탄의 인형</h4>
+					    <span>+상세보기</span>
+					</a>
+					<button type="button" class="btn btn-default">예매하기</button>
+					
+				</li>
+				<li class="col-sm-6 col-md-3">
+					<a href="#">
+						<img src="img/latest3.jpg">
+						<h4>검은 여름</h4>
+					    <span>+상세보기</span>
+					</a>
+					<button type="button" class="btn btn-default">예매하기</button>
+				</li>
+				<li class="col-sm-6 col-md-3">
+					<a href="#">
+						<img src="img/latest4.jpg">
+						<h4>북클럽</h4>
+					    <span>+상세보기</span>
+					</a>
+					<button type="button" class="btn btn-default">예매하기</button>
+				</li>
+			</ul>
+			<ul id="open" class="tab-pane fade">
+				<li class="col-sm-6 col-md-3">
+					<a href="#">
+						<img src="img/prepare1.jpg">
+						<h4>마녀 배달부 키키</h4>
+					    <span>+상세보기</span>
+					</a>
+					<button type="button" class="btn btn-default">예매하기</button>
+				</li>
+				<li class="col-sm-6 col-md-3">
+					<a href="#">
+						<img src="img/prepare2.jpg">
+						<h4> 마녀 배달부 키키 (우리말녹음)</h4>
+					    <span>+상세보기</span>
+					</a>
+					<button type="button" class="btn btn-default">예매하기</button>
+					
+				</li>
+				<li class="col-sm-6 col-md-3">
+					<a href="#">
+						<img src="img/prepare3.jpg">
+						<h4>킬러 인 리커버리</h4>
+					    <span>+상세보기</span>
+					</a>
+					<button type="button" class="btn btn-default">예매하기</button>
+				</li>
+				<li class="col-sm-6 col-md-3">
+					<a href="#">
+						<img src="img/prepare4.jpg">
+						<h4>스파이더맨: 파 프롬 홈</h4>
+					    <span>+상세보기</span>
+					</a>
+					<button type="button" class="btn btn-default">예매하기</button>
+				</li>
+			</ul>
+		</div>
+		<a class="allview btn btn-success btn-sm" href="#">전체보기</a>
+	</article>
+	<article class="container media content2">
+		<div class="media-body col-sm-12 col-md-4">
+			<h3 class="media-heading">hot movie</h3>
+		</div>
+		<div class="media-right col-sm-12 col-md-8">
+			<iframe class="media-object" width="560" height="315" src="https://www.youtube.com/embed/jBdRhhSt3Bc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		</div>
+	</article>
+	<article class="container-fluid jumbotron page-header">
+		<h3>고객센터</h3>
+		<p>1666-2765</p>
+		<p>평일 10:00 - 17:00</p>
+	</article>
+</section>
---
+++ title.php
@@ -0,0 +1,30 @@
+<!-- 타이틀영역 -->
+<div id="title" class="container-fluid">
+	<div id="titleS" class="carousel slide" data-ride="carousel" data-interval="3000"
+	data-pause="hover" data-wrap="true">
+		<ul class="carousel-inner">
+			<li class="item active">
+				<img src="img/op.png" alt="타이틀 이미지1">
+			</li>
+			<li class="item">
+				<img src="img/op.png" alt="타이틀 이미지2">
+			</li>
+			<li class="item">
+				<img src="img/op.png" alt="타이틀 이미지2">
+			</li>
+		</ul>
+		<ol class="carousel-indicators">
+			<li data-target="#titleS" data-slide-to="0" class="active"></li>
+			<li data-target="#titleS" data-slide-to="1"></li>
+			<li data-target="#titleS" data-slide-to="2"></li>
+		</ol>
+		<a href="#titleS" class="left carousel-control" data-slide="prev">
+			<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+			<span class="sr-only">Previous</span>
+		</a>
+		<a href="#titleS" class="right carousel-control" data-slide="next">
+			<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+			<span class="sr-only">Next</span>
+		</a>
+	</div>
+</div>
